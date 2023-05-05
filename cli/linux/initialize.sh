c140b97bcd26776c07f04d12141a472116a35fdd
c0de4un <code4un@yandex.ru>
2023-05-06 01:49:34 +0400

    Add Initialization

---
+++ cli/linux/initialize.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/c0de4un/hex-core.git src/engine/core
+git clone https://github.com/c0de4un/hex-windows.git src/engine/windows
+git clone https://github.com/c0de4un/tetrix-core.git src/app/core
