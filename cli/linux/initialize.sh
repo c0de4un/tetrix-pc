6b036a6ab1bd7019e26ed6c92a7b6607d392ce85
c0de4un <code4un@yandex.ru>
2023-05-14 09:56:50 +0400

    Add installation of hex-gl module

--- cli/linux/initialize.sh
+++ cli/linux/initialize.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/c0de4un/hex-core.git src/engine/core
 git clone https://github.com/c0de4un/hex-windows.git src/engine/windows
+git clone https://github.com/c0de4un/hex-gl.git src/engine/gl
 git clone https://github.com/c0de4un/tetrix-core.git src/app/core
